6ba9a94cfb3f548db5b1603fbbcfa2d7aa99cfe7
dinfekted <infekted@hotmail.de>
2016-09-18 21:34:31 +0530

    ae

---
+++ cppdev/cppdev/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  cppdev
+//
+//  Created by Ashutosh K on 9/18/16.
+//  Copyright © 2016 Ashutosh K. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+int createFileAndGenerateData(){
+    FILE *fp = null;
+    fp = fopen('input_main.dat', 'rw');
+    int r = 0;
+    for (x = 0; i<MAX_INPUT; ++i) {
+        r =
+    }
+}
